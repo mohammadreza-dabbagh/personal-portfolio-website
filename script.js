8be390239a255e96c65f7b1170dbe411de373f80
Mohammadreza Dabbagh98 <MohammadrezaDabbagh1998@gmail.com>
2025-10-02 20:49:28 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const navToggle = document.querySelector('.nav-toggle'); 
+    const navList = document.querySelector('.nav-list');     
+    const navLinks = document.querySelectorAll('.nav-link'); 
 
+    const toggleMenu = () => {
+        navList.classList.toggle('active');
+        
+    };
+
+    navToggle.addEventListener('click', toggleMenu);
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (navList.classList.contains('active')) {
+                
+                toggleMenu(); 
+            }
+        });
+    });
+});
